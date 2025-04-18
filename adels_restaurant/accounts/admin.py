86de4cba3f17db79ff69abd733b7d30de6c6fceb
suadtech <suadtech1@gmail.com>
2025-04-18 09:39:34 +0000

    Admin Configuration

--- adels_restaurant/accounts/admin.py
+++ adels_restaurant/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone_number')
+    search_fields = ('user__username', 'user__email', 'phone_number')
+
--- adels_restaurant/booking/admin.py
+++ adels_restaurant/booking/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Table, Booking
 
-# Register your models here.
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('table_number', 'capacity', 'is_available')
+    list_filter = ('is_available', 'capacity')
+    search_fields = ('table_number',)
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'table', 'booking_date', 'booking_time', 'number_of_guests', 'status')
+    list_filter = ('booking_date', 'status')
+    search_fields = ('user__username', 'user__email')
+    date_hierarchy = 'booking_date'
+
